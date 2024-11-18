8d9a044327af9bb17e9510d6822f3c9922193ec2
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2024-11-18 11:58:21 +0100

    Add auth.py with AuthHandler class structure.

---
+++ auth.py
@@ -0,0 +1,12 @@
+class AuthHandler:
+    def __init__(self):
+        pass
+
+    def register_user(self):
+        pass
+
+    def login_user(self):
+        pass
+
+    def logout_user(self):
+        pass
