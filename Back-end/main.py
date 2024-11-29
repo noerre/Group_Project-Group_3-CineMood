eb77767758e9de854d4d4981a4cf3755d98d9016
JustynaGajdek <131193212+JustynaGajdek@users.noreply.github.com>
2024-11-29 09:41:48 +0100

    Configuring JWT_SECRET_KEY using the SECRET_KEY environment variable

--- Back-end/main.py
+++ Back-end/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import (
+    JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt)
 from dotenv import load_dotenv
 import os
 
@@ -11,6 +13,9 @@ load_dotenv()
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": ["http://localhost", "http://localhost:3000"]}})
+
+app.config['JWT_SECRET_KEY'] = os.getenv("SECRET_KEY")
+jwt = JWTManager(app)
 
 
 @app.route('/')
