e839c62893a26113428fbb5fd3620eb6ce085faf
noerre <hi.noerre@gmail.com>
2024-12-03 19:06:41 +0100

    DB adjustments and test queries
    DATABASE ADJUSTMENTS TO FIT API 
    
    * Deleted auto incremented keys for parameters that can potentially crash with the API fetched parameters (director, actor, genre, movie)
    * Adjust length for actor and director name and deleting the surname to match the way our API stores that info
    * Ingested more countries 
    * Deleted the queries to feed genre, as the API works with its own id and we can retrieve it from there
    * Adjusted length for country names
    * Inserted some mock data in users for local tests

---
+++ sql/test_queries_cinemood.sql
@@ -0,0 +1,23 @@
+/*--------------------------------------
+-- CINEMOOD QUERIES --------------------
+--------------------------------------*/
+
+select * from director;
+select * from movie;
+select * from country;
+select * from mood;
+select * from users;
+
+
+
+
+INSERT INTO users (username, password) VALUES ('john_doe', 'password123');
+INSERT INTO users (username, password) VALUES ('jane_smith', 'mypassword456');
+INSERT INTO users (username, password) VALUES ('alice_wonder', 'alice2023!');
+INSERT INTO users (username, password) VALUES ('bob_marley', 'bobpassword@2024');
+INSERT INTO users (username, password) VALUES ('charlie_brown', 'charlie1234');
+INSERT INTO users (username, password) VALUES ('lucy_lou', 'lucyPassword!99');
+INSERT INTO users (username, password) VALUES ('mike_ross', 'mike2024secure');
+INSERT INTO users (username, password) VALUES ('david_jones', 'david@321');
+INSERT INTO users (username, password) VALUES ('emma_watson', 'emma@2024!');
+INSERT INTO users (username, password) VALUES ('tom_hanks', 'tomHanks!password');
